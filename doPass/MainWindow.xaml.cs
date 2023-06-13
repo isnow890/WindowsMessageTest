ff82f4a1e0ee6402aab7ab695039a1aa91b43c9b
양찬우 <isnow890@hotmail.com>
2023-06-13 13:49:19 +0900

    Add project files.

---
+++ doPass/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace doPass
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private uint message;
+        private IntPtr handle;
+        UInt32 sSendNo;
+
+        public const uint HWND_BROADCAST = 0xffff;
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern uint RegisterWindowMessage(string lpString);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool PostMessage(IntPtr hWnd, uint Msg, uint wParam, uint lParam);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            handle = new WindowInteropHelper(this).Handle;
+            message = RegisterWindowMessage("EUMCHIS");
+
+        }
+
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            sSendNo = Convert.ToUInt32(name.Text);
+            PostMessage((IntPtr)HWND_BROADCAST, message, (uint)handle, (uint)sSendNo);
+        }
+    }
+}
---
+++ get/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace get
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private uint message;
+        private IntPtr handle;
+        UInt32 sSendNo;
+
+        public const uint HWND_BROADCAST = 0xffff;
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern uint RegisterWindowMessage(string lpString);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern bool PostMessage(IntPtr hWnd, uint Msg, uint wParam, uint lParam);
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            handle = new WindowInteropHelper(this).Handle;
+            message = RegisterWindowMessage("EUMCHIS");
+
+            ComponentDispatcher.ThreadFilterMessage += new ThreadMessageEventHandler(ComponentDispatcher_ThreadFilterMessage);
+        }
+
+
+        void ComponentDispatcher_ThreadFilterMessage(ref MSG msg, ref bool handled)
+        {
+            if (msg.message == message && msg.wParam != handle)
+            {
+                name.Text = msg.lParam.ToString();
+            }
+        }
+
+
+
+    }
+}
